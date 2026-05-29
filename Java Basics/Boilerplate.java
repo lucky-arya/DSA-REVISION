501ab4aff1063a1763ef7fc8cec4f95ef1dfcecc
luckt-arya <lklk112111@gmail.com>
2026-05-29 23:26:52 +0530

    change in boilerplate code

--- Java Basics/Boilerplate.java
+++ Java Basics/Boilerplate.java
@@ -1,9 +1,13 @@
 import java.util.*;
 public class Boilerplate {
+
+    // public return_type method_name(parameters) {
+    //     // write code here
+    // }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Boilerplate bp = new Boilerplate();
-        // write code here
+        // bp.method_name(arguments);
         sc.close();
     }
 }
